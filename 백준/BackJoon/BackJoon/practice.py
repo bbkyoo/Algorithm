aeb4aea645fea8069f2e807d5fb8d9bddea037a0
bbkyoo <bbkyoo@naver.com>
2021-10-21 22:53:41 +0900

    update Algorithm
    update Algorithm

--- 백준/BackJoon/BackJoon/practice.py
+++ 백준/BackJoon/BackJoon/practice.py
@@ -1,17 +1,36 @@
-n = int(input())
-arr = list(map(int, input().split()))
-maxV = 0
+import heapq
+import sys
+inf = sys.maxsize
 
-def dfs(depth):
-    if len(arr) == 2:
-        if depth > maxV:
-            maxV = depth
-        return
+n, e = map(int, input().split())
+s = [[] for _ in range(n+1)]
 
-    for i in range(1, len(arr)-1):
-        r = arr[i-1] * arr[i+1]
-        temp = arr[i]
-        del arr[i]
-        dfs(depth+r)
-        arr.insert(i, temp)
+for _ in range(e):
+    a, b, c = map(int, input().split())
+    s[a].append([c, b])
+    s[b].append([c, a])
+    
+def dijkstra(start):
+    dp = [inf]*(n+1)
+    dp[start] = 0
+    heap = []
+    heapq.heappush(heap, [0, start])
 
+    while heap:
+        w, n = heapq.heappop(heap)
+
+        for wei, n_n in s[n]:
+            next_wei = wei + w
+            if next_wei < dp[n_n]:
+                dp[n_n] = next_wei
+                heapq.heappush(heap, [next_wei, n_n])
+
+    return dp
+
+v1, v2 = map(int, input().split())
+
+one = dijkstra(1)
+v1_ = dijkstra(v1)
+v2_ = dijkstra(v2)
+
+cnt = min(one[v1]+v1_[v2]+v2_[n], one[v2]+v2_[v1]+v1_[n])
